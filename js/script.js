e31ad8c07c70154d2b8672207b5d09eda4645241
Giga_Dev148 <gigakatamadze0@gmail.com>
2023-04-22 17:00:13 +0400

    made smooth scrolling for safari browser

--- js/script.js
+++ js/script.js
@@ -29,7 +29,24 @@ const allLinks = document.querySelectorAll("a:link");
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
     e.preventDefault();
-    const href = l
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href == "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    // scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      console.log(sectionEl);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // close mobile application
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
   });
 });
 
